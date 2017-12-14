f207ab30481344c91b2389e5ecf3e9386c1e644f
Alex Hoppe <amhoppe@Alexs-MBP.T-mobile.com>
2017-12-13 22:39:43 -0500

    first commit no video

---
+++ assets/javascript/hangman.js
@@ -0,0 +1,17 @@
+// Hangman Javascript
+
+// need to initialize an array of words to use in game
+
+//individual word is chosen
+// word is parsed and turned into an array
+// array is assigned to letters/blocks
+// blocks are hidden
+// ask for user input
+// compare user input with word chosen
+// if statement to determine how the choices compare
+// if true reveal letter in word
+// if not true print to incorrect letters
+// if not true move to another image or progression in hangman
+// if not true increase hangman counter by one
+// hangman game runs until counter reaches 5
+// if counter reaches 5 print you lose refresh the page to play again
---
+++ assets/javascript/psychic.js
@@ -0,0 +1,27 @@
+// Zoltar Psychic Game //
+
+// Actions: Zoltar Picks a letter from the alphabet 
+//			User enter's a letter
+//			User and Zoltar picks are compared using if statement
+//			Win or loss determined
+//			Write used letters into page
+// 			record win/loss
+// 			restart page
+
+// initialize arrary of alphabet
+
+var alpha = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
+
+// Zoltar picks a random number from 0-25
+
+// Zoltar calls array indexed to his choice
+
+// user input is recorded
+
+//user input and zoltar choice are compared
+
+//if statement is used to compare inputs and then write to wins and losses column
+
+// user input is recorded in div at the bottom of the page
+
+// game is restarted
